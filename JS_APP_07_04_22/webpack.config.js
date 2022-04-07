9242c03e523363cef6110d60d2a7db5bb0f9b8b4
timofeyPython <tima.tepaev@mail.ru>
2022-04-07 23:27:31 +0300

    app

--- JS_APP_07_04_22/webpack.config.js
+++ JS_APP_07_04_22/webpack.config.js
@@ -1,12 +1,21 @@
 const path = require('path')//Работаем с путями (папки)
+const HtmlPlugin = require('html-webpack-plugin')//подлкючаем плагин
 
 module.exports = {
     entry:'./src/app.js',
     output:{
-        filename: 'bundle.js',
+        filename: 'bundle.[chunkhash].js',
         path:path.resolve(__dirname,'public')//складываем все JS скрипты в папку 
     },
     devServer:{
         port:3000
-    }
+    },
+    plugins:[
+        new HtmlPlugin({
+            template: './src/index.html'
+        })
+    ]
+
+    
+    
 }
