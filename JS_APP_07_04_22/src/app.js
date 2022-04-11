1f89bf409724cf984f79a4b75a8a1e63c6694de5
timofeyPython <tima.tepaev@mail.ru>
2022-04-11 21:28:59 +0300

    test

--- JS_APP_07_04_22/src/app.js
+++ JS_APP_07_04_22/src/app.js
@@ -1,7 +1,24 @@
-//  npm init 
+import {isValid} from "./utils";
 import './style.css'
-const form = document.getElementsByName('form')
-const input = document.getElementsByName('question-input')
-const submitBtn = document.getElementsByName('submit')
 
-console.log('hello')
+
+const form = document.getElementById('form')
+//когда селектор #
+const input = document.querySelector('#question-input')
+const submitBtn = document.querySelector('#submit')
+
+
+form.addEventListener('submit', submitFormHandler)
+
+function submitFormHandler  (event) {
+    event.preventDefault()
+    // вызываем валидатор ,если объект валидный
+    if(isValid(input.value)){
+        const question = {
+            //trim удаляем лишние пробелы
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+        // Async request to server to save question
+    }
+}
