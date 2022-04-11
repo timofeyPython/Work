3f4c4bf0a2316713654b54e91b4befca2c530592
timofeyPython <tima.tepaev@mail.ru>
2022-04-11 20:53:08 +0300

    job app

--- JS_APP_07_04_22/src/app.js
+++ JS_APP_07_04_22/src/app.js
@@ -1,3 +1,7 @@
 //  npm init 
+import './style.css'
+const form = document.getElementsByName('form')
+const input = document.getElementsByName('question-input')
+const submitBtn = document.getElementsByName('submit')
 
 console.log('hello')
