8384e63a6be9bb9adb921e81f8c33f85d55ad4b6
timofeyPython <tima.tepaev@mail.ru>
2022-04-24 13:59:23 +0300

    настройка

--- JS_PRO/src/index.js
+++ JS_PRO/src/index.js
@@ -1,2 +1,2 @@
 import './scss/index.scss'
-console.log('Hello')
+console.log('Hello My Xui')
--- JS_PRO/webpack.config.js
+++ JS_PRO/webpack.config.js
@@ -1,56 +1,91 @@
-const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
-module.exports = {
-    context: path.resolve(__dirname,'src'),
-    entry: {
-        main:['core-js/stable',
-              'regenerator-runtime/runtime',
-              './index.js']
-    },
-    output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: '[name].bundle.js'
-    },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@':path.resolve(__dirname,'src'),
-            '@core':path.resolve(__dirname,'src','core'),
-        }
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./index.html"
-    }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname,'src','favicon.ico'),
-                    to: path.resolve(__dirname,'dist')
-                },
-            ],
-        }),
-        new MiniCssExtractPlugin({
-            filename:'[name].bundle.css'
-        })
+module.exports = (env, argv) => {
+    const isProd = argv.mode === 'production'
+    const isDev = !isProd
 
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    MiniCssExtractPlugin.loader,
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
+    console.log('isProd', isProd)
+    console.log('isDev', isDev)
+
+    const filename = (ext) =>
+        isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+
+    const plugins = () => {
+        const base = [
+            new HtmlWebpackPlugin({
+                template: './index.html'
+            }),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, 'src', 'favicon.ico'),
+                        to: path.resolve(__dirname, 'dist')
+                    },
                 ],
-            },
-        ],
-    },
+            }),
+            new MiniCssExtractPlugin({
+                filename: filename('css')
+            }),
+            new CleanWebpackPlugin(),
+        ]
 
+        if (isDev) {
+            base.push(new ESLintPlugin())
+        }
+
+        return base
+    }
+
+    return {
+        target: 'web',
+        context: path.resolve(__dirname, 'src'),
+        entry: {
+            main: './index.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: filename('js')
+        },
+        resolve: {
+            extensions: ['.js'],
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@core': path.resolve(__dirname, 'src', 'core'),
+            }
+        },
+        devServer: {
+            port: '3000',
+            open: true,
+            hot: true,
+        },
+        devtool: isDev ? 'source-map' : false,
+        plugins: plugins(),
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ],
+        }
+    }
 }
